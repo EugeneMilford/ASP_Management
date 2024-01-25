d75b29636f9992284369091f9809a9b9ace80e99
Eugene Milford <gmilford321@gmail.com>
2024-01-25 12:43:09 +0200

    Projects Added

---
+++ Pages/OfficeProjects/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OfficeManagement.Data;
+using OfficeManagement.Models;
+
+namespace OfficeManagement.Pages.OfficeProjects
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly OfficeManagement.Data.OfficeContext _context;
+
+        public DeleteModel(OfficeManagement.Data.OfficeContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Project Project { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Projects == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects.FirstOrDefaultAsync(m => m.ProjectId == id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Project = project;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Projects == null)
+            {
+                return NotFound();
+            }
+            var project = await _context.Projects.FindAsync(id);
+
+            if (project != null)
+            {
+                Project = project;
+                _context.Projects.Remove(Project);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
