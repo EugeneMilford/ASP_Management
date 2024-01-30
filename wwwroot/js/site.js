37e5c1690a15c920d67cc448193800281086b2b1
Eugene Milford <gmilford321@gmail.com>
2024-01-30 18:12:50 +0200

    Extra UI Pages Added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        selectable: true,
+        dateClick: function (info) {
+            var entry = prompt('Enter your entry for ' + info.dateStr);
+            if (entry) {
+                // Here you can add your logic to save the entry
+                // For demo, let's just log it to console
+                console.log('Entry for ' + info.dateStr + ': ' + entry);
+            }
+        }
+    });
+    calendar.render();
+});
